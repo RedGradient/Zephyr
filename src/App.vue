0ed3883d059cb9888d4f34aa15d6b370c8decd32
RedGradient <a51ori9w@tutanota.com>
2021-10-16 21:20:20 +1100

    minor reorganization of components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Auth from './components/auth/Auth.vue'
+
 </script>
 
 <template>
-  <Auth />
+
+  <router-view></router-view>
+  
 </template>
 
 <style>
